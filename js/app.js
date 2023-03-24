047c23d10d7d274e2b044eedbecb899252066539
Philippe DELENTE <80407535+PhilDaiguille@users.noreply.github.com>
2023-03-24 13:09:40 +0100

    add ReactJS

--- js/app.js
+++ js/app.js
@@ -1,26 +1,15 @@
-import data from "../json/data.js"
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("charged");
-  console.log(data)
-  let affiche = document.querySelector("html body main div.film");
-  for(let i = 0; i <= data.films.length; i++){
-    affiche.innerHTML += `
-      <section>
-	  	<figure>
-        	<img src="${data.films[i].image}" alt="${data.films[i].title}">
-			<figcaption>
-        		<h2>${data.films[i].title}</h2>
-        		<time>${data.films[i].release_date}</time>
-        		<h3>Director : ${data.films[i].director}</h3>
-        		<h3>Producer : ${data.films[i].producer}</h3>
-        		<p>${data.films[i].description}</p>
-        		<p><span class="material-symbols-outlined">
-				star
-				</span>${data.films[i].rt_score} </p>
-			</figcaption>
-		</figure>
-      </section>
-    `;
-  }
-//   <img src="${data.films[i].movie_banner}" alt="${data.films[i].title}"></img>
-});
+import data from "../json/data.js";
+
+const Film = ({ film }) => {
+  return React.createElement("section", null, React.createElement("figure", null, React.createElement("img", { src: film.image, alt: film.title }), React.createElement("figcaption", null, React.createElement("h2", null, film.title), React.createElement("time", null, film.release_date), React.createElement("h3", null, `Director : ${film.director}`), React.createElement("h3", null, `Producer : ${film.producer}`), React.createElement("p", null, film.description), React.createElement("p", null, [
+  // Ajout d'une prop key unique à l'élément span
+  React.createElement("span", { className: "material-symbols-outlined", key: `${film.id}-star` }, "star"), `${film.rt_score}`]))));
+};
+
+const App = () => {
+  return data.films.map(film => {
+    return React.createElement(Film, { key: film.id, film: film });
+  });
+};
+
+ReactDOM.render(React.createElement(App), document.querySelector(".film"));
